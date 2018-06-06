f9942f7fe71241f73118dcd518218f990e4e6092
Nanelia <alicia.nanelia@outlook.com>
2018-06-06 14:50:55 +0800

    added in some tests and pg gem for postgres db.

--- spec/foodie_spec.rb
+++ spec/foodie_spec.rb
@@ -1,9 +1,21 @@
+require "foodie"
+
 RSpec.describe Foodie do
   it "has a version number" do
     expect(Foodie::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
+end
+
+RSpec.describe Foodie::Food do
+  it "broccoli is gross" do
+    expect(Foodie::Food.portray("Broccoli")).to eql("Gross!")
+  end
+
+  it "anything else is delicious" do
+    expect(Foodie::Food.portray("Not Broccoli")).to eql("Delicious!")
   end
 end
